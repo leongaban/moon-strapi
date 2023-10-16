d8d9930032c0d79743b02321894c6482879a24c5
leongaban <leongaban@gmail.com>
2023-10-15 22:07:16 -0500

    updated config env database.js

--- config/env/database.js
+++ config/env/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      host: env("DATABASE_HOST", `${process.env.HOST}`),
+      port: env.int("DATABASE_PORT", `${process.env.PORT}`),
+      database: env("DATABASE_NAME", `${process.env.DB_NAME}`),
+      user: env("DATABASE_USERNAME", `${process.env.USER}`),
+      password: env("DATABASE_PASSWORD", `${process.env.PASSWORD}`),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         ca: env("DATABASE_CA"),
